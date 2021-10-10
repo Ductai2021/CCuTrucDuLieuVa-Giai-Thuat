0370099fafb418cc7c5e18f31eed964689a801ce
CTDL-CTDLTT <nguyenductaibchau@gmail.gmailcom>
2021-10-10 23:09:43 +0700

    cập nhật mã code

---
+++ src/com/Taind2008110304/Tuan04/Maytinh.java
@@ -0,0 +1,28 @@
+package com.Taind2008110304.Tuan04;
+
+public class Maytinh {
+    public class MayTinh {
+        String NSX, HDH, ram, CPU;
+        int SX, gia, BH;
+
+        MayTinh(String nhasx, String hdh, String r, String cpu, int namsx, int g, int nBH) {
+            NSX = NSX;
+            HDH = HDH;
+            ram = ram;
+            CPU = CPU;
+            SX = SX;
+            gia = gia;
+            BH = BH;
+        }
+
+        void inthongtin() {
+            System.out.println("Nhà sản xuất: " + NSX);
+            System.out.println("Hệ điều hành: " + HDH);
+            System.out.println("Ram: " + ram);
+            System.out.println("CPU: " + CPU);
+            System.out.println("Năm sản xuất: " + SX);
+            System.out.println("Giá: " + gia);
+            System.out.println("Năm bảo hành: " + BH);
+        }
+    }
+}
