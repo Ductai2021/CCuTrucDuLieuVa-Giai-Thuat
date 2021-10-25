7ce357afdd6fe27b64b6526d04fff1d2f5e4d0a4
CTDL-CTDLTT <nguyenductaibchau@gmail.gmailcom>
2021-10-25 12:31:39 +0700

    nộp mã code

---
+++ src/com/Taind2008110304/kiemtragiuaky/Apple.java
@@ -0,0 +1,35 @@
+package com.Taind2008110304.kiemtragiuaky;
+
+import java.util.Scanner;
+
+public class Apple {
+    int id;
+    int size;
+    String color;
+    int nhap;
+
+    Scanner n = new Scanner(System.in);
+
+    Apple(int i, int s, String cl) {
+        id = i;
+        size = s;
+        color = cl;
+    }
+
+    void inThongTin() {
+
+        System.out.println("mã id: " + id);
+        System.out.println("màu sắc: " + color);
+        System.out.println("kích thước: " + size);
+    }
+
+    void nhapThongTin() {
+        System.out.print("Nhập mã id: ");
+        id = n.nextInt();
+        System.out.println("Nhập kích thước: ");
+        size = n.nextInt();
+        System.out.print("Màu sắc: ");
+        color = n.nextLine();
+    }
+
+}
