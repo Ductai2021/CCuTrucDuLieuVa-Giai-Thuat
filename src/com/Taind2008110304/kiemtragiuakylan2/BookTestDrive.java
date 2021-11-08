eacfe618cb1fac8ba30df0b0b8e61c88e9aeb15a
CTDL-CTDLTT <nguyenductaibchau@gmail.gmailcom>
2021-11-08 11:36:21 +0700

    cập nhật link git kiểm tra giữa kì lần 2

--- src/com/Taind2008110304/kiemtragiuakylan2/BookTestDrive.java
+++ src/com/Taind2008110304/kiemtragiuakylan2/BookTestDrive.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class BookTestDrive {
     public static void main(String[] args) {
         Scanner book = new Scanner(System.in);
-         // a)
-         System.out.print("Nhập số lượng sách : ");
-         int sL = book.nextInt();
-         BookArrayList danhsachbook = new BookArrayList(sL);
-         danhsachbook.NhapDS();
- 
-         danhsachbook.inDanhSachBook();
-         // b)
-         System.out.println("Nhập vị trí cần thêm vào : ");
-         int viTriThem = book.nextInt();
-         book.nextLine();
-         danhsachbook.themBook(viTriThem);
- 
-         System.out.println("Danh sách sau khi thêm :");
-         danhsachbook.inDanhSachBook();
-         // c)
-         // d)
-         danhsachbook.inDanhSachBook();
-     }
-}
+        // a)
+        System.out.print("Nhập số lượng sách : ");
+        int sL = book.nextInt();
+        BookArrayList danhsachbook = new BookArrayList(sL);
+        danhsachbook.NhapDS();
 
+        danhsachbook.inDanhSachBook();
+        // b)
+        System.out.println("Nhập vị trí cần thêm vào : ");
+        int viTriThem = book.nextInt();
+        book.nextLine();
+        danhsachbook.themBook(viTriThem);
+
+        System.out.println("Danh sách book sau khi thêm :");
+        danhsachbook.inDanhSachBook();
+        // c)
+        System.out.println("Nhập mã id sách cần tìm : ");
+        String tim = book.nextLine();
+        danhsachbook.findTheoid(tim);
+        // d)
+        danhsachbook.inDanhSachBook();
+    }
 }
